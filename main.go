c151e4cca0f084f981d92928f2d64d950bca69b5
Yves Brissaud <eunomie@squarescale.com>
2017-10-05 23:51:59 +0200

    Expose metrics for prometheus

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/eunomie/github-board-exporter/github"
 	log "github.com/sirupsen/logrus"
+)
+
+const (
+	issueMetricsPattern = "issues{column=\"%s\"} %d"
 )
 
 func main() {
@@ -18,22 +26,34 @@ func main() {
 		log.Fatalf("could not create Github client: %v", err)
 	}
 
-	project, err := github.NewProject(conf.projectID, g)
-	if err != nil {
-		log.Fatalf("could not read info for project %d: %v", conf.projectID, err)
-	}
+	m := metrics(g, conf.projectID)
+	http.HandleFunc("/metrics", m)
+	http.ListenAndServe(":8080", nil)
+}
 
-	log.Println(project.ID)
-
-	log.Println(project.Columns.GetByName("📚 Backlog"))
-	for _, col := range project.Columns {
-		log.Println(col.Name)
-		for _, card := range col.Cards {
-			if card.Note != "" {
-				log.Println("  " + card.Note)
-			} else {
-				log.Println("  " + card.ContentURL)
-			}
+func numberOfIssues(cards []github.Card) int {
+	n := 0
+	for _, card := range cards {
+		if card.ContentURL != "" {
+			n++
 		}
+	}
+	return n
+}
+
+func metrics(g *github.Github, id int) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		p, err := github.NewProject(id, g)
+		if err != nil {
+			errString := fmt.Sprintf("could not read project %d: %v", id, err)
+			log.Errorln(errString)
+			http.Error(w, errString, http.StatusInternalServerError)
+		}
+		metrics := []string{}
+		for _, col := range p.Columns {
+			metric := fmt.Sprintf(issueMetricsPattern, col.Name, numberOfIssues(col.Cards))
+			metrics = append(metrics, metric)
+		}
+		fmt.Fprintln(w, strings.Join(metrics, "\n"))
 	}
 }
