b4cf463aadcdda1a732deb6a64c7949f006d1d83
Yves Brissaud <eunomie@squarescale.com>
2017-10-06 19:27:32 +0200

    Export pr metrics

--- main.go
+++ main.go
@@ -29,28 +29,41 @@ func main() {
 
 	c := cache{}
 
-	m := metrics(g, conf.projectID, &c)
+	m := metrics(g, conf.projectID, conf.user, &c)
 	http.HandleFunc("/metrics", m)
 	http.ListenAndServe(":8080", nil)
 }
 
-func metrics(g *github.Github, id int, c *cache) func(w http.ResponseWriter, r *http.Request) {
+func metrics(g *github.Github, id int, u string, c *cache) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("ask metrics")
 		if c.Metrics == "" || c.RefreshedAt.Before(time.Now().Add(-30*time.Minute)) {
 			log.Println("  fetch project")
 			c.RefreshedAt = time.Now()
 
-			p, err := github.NewProject(id, g)
+			m, err := allMetrics(id, u, g)
 			if err != nil {
-				errString := fmt.Sprintf("could not read project %d: %v", id, err)
-				log.Errorln(errString)
-				http.Error(w, errString, http.StatusInternalServerError)
+				log.Errorln(err)
+				http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 			}
-			log.Println("  project fetched")
-			c.Metrics = p.Metrics()
+			c.Metrics = m
 		}
 		fmt.Fprintln(w, c.Metrics)
 		log.Println("end metrics")
 	}
+}
+
+func allMetrics(id int, u string, g *github.Github) (string, error) {
+	p, err := github.NewProject(id, g)
+	if err != nil {
+		return "", fmt.Errorf("could not read project %d: %v", id, err)
+	}
+
+	pr, err := github.PullRequestsMetrics(g, u)
+	if err != nil {
+		return "", fmt.Errorf("could not read pull request metrics for user %s: %v", u, err)
+	}
+
+	metrics := fmt.Sprintf("%s\n%s", p.Metrics(), pr)
+	return metrics, nil
 }
