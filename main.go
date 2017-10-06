174d97347827a58c1ddfe7992b9b4d0900249a9b
Yves Brissaud <eunomie@squarescale.com>
2017-10-06 15:34:54 +0200

    Add basic simple cache with 30m retention

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/eunomie/github-board-exporter/github"
 	log "github.com/sirupsen/logrus"
 )
+
+type cache struct {
+	RefreshedAt time.Time
+	Metrics     string
+}
 
 func main() {
 	conf, err := newConfiguration()
@@ -21,22 +27,30 @@ func main() {
 		log.Fatalf("could not create Github client: %v", err)
 	}
 
-	m := metrics(g, conf.projectID)
+	c := cache{}
+
+	m := metrics(g, conf.projectID, &c)
 	http.HandleFunc("/metrics", m)
 	http.ListenAndServe(":8080", nil)
 }
 
-func metrics(g *github.Github, id int) func(w http.ResponseWriter, r *http.Request) {
+func metrics(g *github.Github, id int, c *cache) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("ask metrics")
-		p, err := github.NewProject(id, g)
-		if err != nil {
-			errString := fmt.Sprintf("could not read project %d: %v", id, err)
-			log.Errorln(errString)
-			http.Error(w, errString, http.StatusInternalServerError)
+		if c.Metrics == "" || c.RefreshedAt.Before(time.Now().Add(-30*time.Minute)) {
+			log.Println("  fetch project")
+			c.RefreshedAt = time.Now()
+
+			p, err := github.NewProject(id, g)
+			if err != nil {
+				errString := fmt.Sprintf("could not read project %d: %v", id, err)
+				log.Errorln(errString)
+				http.Error(w, errString, http.StatusInternalServerError)
+			}
+			log.Println("  project fetched")
+			c.Metrics = p.Metrics()
 		}
-		log.Println("  project fetched")
-		fmt.Fprintln(w, p.Metrics())
+		fmt.Fprintln(w, c.Metrics)
 		log.Println("end metrics")
 	}
 }
