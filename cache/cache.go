5473c7c91ea2a0bbbd4d9f964725bab5abe62a6c
Yves Brissaud <eunomie@squarescale.com>
2017-10-13 14:38:14 +0200

    Extract cache and refresh automatically

---
+++ cache/cache.go
@@ -0,0 +1,50 @@
+package cache
+
+import (
+	"fmt"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Cache store a content during some caching duration
+type Cache struct {
+	caching time.Duration
+	refresh func() (string, error)
+	Content string
+}
+
+// NewCache creates a new cache object with refresh method
+func NewCache(caching time.Duration, refresh func() (string, error)) *Cache {
+	c := Cache{
+		caching: caching,
+		refresh: refresh,
+		Content: "",
+	}
+	c.autoRefreshContent()
+	c.refreshContent()
+	return &c
+}
+
+func (c *Cache) autoRefreshContent() {
+	ticker := time.NewTicker(c.caching)
+	go func() {
+		for _ = range ticker.C {
+			if err := c.refreshContent(); err != nil {
+				log.Errorf("could not auto refresh content: %v", err)
+			}
+		}
+	}()
+}
+
+func (c *Cache) refreshContent() error {
+	log.Println("refresh content")
+
+	content, err := c.refresh()
+	if err != nil {
+		return fmt.Errorf("could not refresh content: %v", err)
+	}
+	c.Content = content
+
+	return nil
+}
--- main.go
+++ main.go
@@ -5,14 +5,10 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/eunomie/github-board-exporter/cache"
 	"github.com/eunomie/github-board-exporter/github"
 	log "github.com/sirupsen/logrus"
 )
-
-type cache struct {
-	RefreshedAt time.Time
-	Metrics     string
-}
 
 func main() {
 	conf, err := newConfiguration()
@@ -27,43 +23,32 @@ func main() {
 		log.Fatalf("could not create Github client: %v", err)
 	}
 
-	c := cache{}
-
-	m := metrics(g, conf.projectID, conf.user, &c)
-	http.HandleFunc("/metrics", m)
+	c := cache.NewCache(30*time.Minute, allMetrics(conf.projectID, conf.user, g))
+	http.HandleFunc("/metrics", metrics(c))
 	http.ListenAndServe(":8080", nil)
 }
 
-func metrics(g *github.Github, id int, u string, c *cache) func(w http.ResponseWriter, r *http.Request) {
+func metrics(c *cache.Cache) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("ask metrics")
-		if c.Metrics == "" || c.RefreshedAt.Before(time.Now().Add(-30*time.Minute)) {
-			log.Println("  fetch project")
-			c.RefreshedAt = time.Now()
-
-			m, err := allMetrics(id, u, g)
-			if err != nil {
-				log.Errorln(err)
-				http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-			}
-			c.Metrics = m
-		}
-		fmt.Fprintln(w, c.Metrics)
-		log.Println("end metrics")
+		content := c.Content
+		fmt.Fprintln(w, content)
 	}
 }
 
-func allMetrics(id int, u string, g *github.Github) (string, error) {
-	p, err := github.NewProject(id, g)
-	if err != nil {
-		return "", fmt.Errorf("could not read project %d: %v", id, err)
-	}
+func allMetrics(id int, u string, g *github.Github) func() (string, error) {
+	return func() (string, error) {
+		p, err := github.NewProject(id, g)
+		if err != nil {
+			return "", fmt.Errorf("could not read project %d: %v", id, err)
+		}
 
-	pr, err := github.PullRequestsMetrics(g, u)
-	if err != nil {
-		return "", fmt.Errorf("could not read pull request metrics for user %s: %v", u, err)
-	}
+		pr, err := github.PullRequestsMetrics(g, u)
+		if err != nil {
+			return "", fmt.Errorf("could not read pull request metrics for user %s: %v", u, err)
+		}
 
-	metrics := fmt.Sprintf("%s\n%s", p.Metrics(), pr)
-	return metrics, nil
+		metrics := fmt.Sprintf("%s\n%s", p.Metrics(), pr)
+		return metrics, nil
+	}
 }
